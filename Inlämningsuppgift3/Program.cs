c6c1a3b73be3972076c1ce1608959310abde6595
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-23 10:37:26 +0200

    Add project files.

---
+++ Inlämningsuppgift3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämningsuppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ SharedLibraries/Services/DeviceService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibraries.Services
+{
+    public static class DeviceService
+    {
+
+    }
+}
